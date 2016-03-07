7e96fc9ee3b8a21dbf0e10e6e5525fc79d9c9a42
Aki Vehtari <Aki.Vehtari@aalto.fi>
2016-03-07 12:16:35 +0200

    show also result without "outliers"

--- demos_ch6/demo6_4.m
+++ demos_ch6/demo6_4.m
@@ -16,10 +16,26 @@ s=sqrt(s2);
 my=mean(y);
 
 % tail area probabilities of marginal predictive distributions
+subplot(2,1,1)
 Ty=t_cdf(y,n-1,my,sqrt(1+1/n).*s);
-hist(Ty,0.05:.05:.95)
+hist(Ty,0.05:.1:.95)
 title({'Light speed example','distribution of marginal posterior p-values'})
 % Histogram should look uniformish, if the posterior predictive
 % distributions are well calibrated (this test is more reliable with
 % cross-validation predictive distributions).
 
+pause
+
+% Let's remove two "outliers"
+y([6 10])=[];
+n=length(y);
+s2=var(y);
+s=sqrt(s2);
+my=mean(y);
+
+% tail area probabilities of marginal predictive distributions
+subplot(2,1,2)
+Ty=t_cdf(y,n-1,my,sqrt(1+1/n).*s);
+hist(Ty,0.05:.1:.95)
+title({'After removing "outliers"','distribution of marginal posterior p-values'})
+% Histogram looks more uniformish after removing two "outliers"
